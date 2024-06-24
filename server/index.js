058e9c9aff779e0d107b4cbc27e7e54195df6e8e
Israel Albuquerque <israel.albuquerque@sillis.com.br>
2024-06-23 21:45:36 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 const app = express();
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT || 4000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
